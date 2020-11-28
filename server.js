eb45d6b9b92d3a030238d7feb5758413a5a4536c
jtG <jude.gyimah87@gmail.com>
2020-11-28 13:35:56 +0100

    signin register image update

--- server.js
+++ server.js
@@ -3,9 +3,82 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const database = {
+    'users':[
+        {
+            'id': '123',
+            'name': 'John',
+            'email': 'john@gmail.com',
+            'password': 'cookies',
+            'entries': 0,
+            'joined': new Date()
+        },
+        {
+            'id': '124',
+            'name': 'Sally',
+            'email': 'sal@gmail.com',
+            'password': 'milk',
+            'entries': 0,
+            'joined': new Date()
+        }
+]
+}
+
 app.get('/', (req, res)=>{
 
-    res.send('This is live');
+    res.json(database.users);
+
+});
+
+app.post('/signin',(req, res)=>{
+
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json('success');
+    }else{
+        res.status(400).json('error logging in')
+    }
+
+});
+
+app.post('/register', (req, res)=>{
+    const { email, password, name } = req.body;
+
+    database.users.push({
+        'id': '125',
+        'name': name,
+        'email': email,
+        'password': password,
+        'entries': 0,
+        'joined': new Date()
+    });
+
+    res.json(database.users[database.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res)=>{
+
+        const { id } = req.params;
+        const profile = database.users.filter(user =>{
+            if(user.id === id){
+                return user;
+            }
+        });
+
+        profile.length > 0 ? res.json(profile) : res.status(404).json('user not found');
+
+});
+
+app.put('/image', (req, res)=>{
+
+        const { id } = req.body;
+        const profile = database.users.filter(user =>{
+            if(user.id === id){
+                user.entries++;
+                return user;
+            }
+        });
+
+        profile.length > 0 ? res.json(profile[0].entries) : res.status(404).json('user not found');
 
 });
 
